6c0cfead8c349c34580f7ed2e263ef15af07e88a
h-e-l-e-n <leeleilei07@gmail.com>
2024-12-15 20:53:45 +0800

    add path

--- app.js
+++ app.js
@@ -1,16 +1,19 @@
 import Express from 'express'
 import v1Route from './routes/v1.js'
+import path from 'path'
+import { fileURLToPath } from 'url';
 
-
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 const app = Express()
 app.use(Express.urlencoded({ extended: true })); // 處理 URL 編碼的表單數據
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs')
 
 app.use(Express.json())
 app.use("/", v1Route)
-app.set('view engine', 'ejs')
-app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'public', 'index.html'));
-});
+app.use(Express.static(path.join(__dirname, 'public')));
+
 
 
 const PORT = process.env.PORT || 3300; // 預設本地測試使用 3000
