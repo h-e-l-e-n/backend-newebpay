0abf811cb6472eb718666a9f7b297ab60f7937c8
h-e-l-e-n <leeleilei07@gmail.com>
2024-12-15 21:57:58 +0800

    add get /

--- routes/v1.js
+++ routes/v1.js
@@ -2,7 +2,11 @@ import Express from 'express'
 import * as PaymentController from '../controllers/payment_controller.js'
 
 const v1Route = Express.Router()
-
+v1Route.get('/', (req, res) => {
+    res.send('Welcome to the API')
+    // 或
+    // res.render('index')
+  })
 v1Route.get('/payment', PaymentController.Payment)
 v1Route.post('/createOrder', PaymentController.CreateOrder)
 v1Route.get('/check/:id', PaymentController.CheckDetail)
