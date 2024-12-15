86e1d0e255e8b4ba9b7cfa727ecafa40ab6029c4
h-e-l-e-n <leeleilei07@gmail.com>
2024-12-15 21:53:50 +0800

    adjust app.js

--- app.js
+++ app.js
@@ -5,25 +5,36 @@ import { fileURLToPath } from 'url';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
+
 const app = Express()
-app.use(Express.urlencoded({ extended: true })); // 處理 URL 編碼的表單數據
+
+// 中介軟體
+app.use(Express.urlencoded({ extended: true }));
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs')
-
 app.use(Express.json())
-app.use("/", v1Route)
 app.use(Express.static(path.join(__dirname, 'public')));
 
+// 設定 header
 app.use((req, res, next) => {
-    res.setHeader('Accept-Ranges', 'none');
-    next();
+  res.setHeader('Accept-Ranges', 'none');
+  next();
 });
 
+// 路由
+app.use("/", v1Route)
 
-const PORT = process.env.PORT || 3300; // 預設本地測試使用 3000
-app.listen(PORT, () => {
+// 處理未定義路由的中介軟體
+app.use((req, res) => {
+  res.status(404).send('Not Found');
+});
+
+// 僅在本地開發時啟動伺服器
+if (process.env.NODE_ENV !== 'production') {
+  const PORT = process.env.PORT || 3300;
+  app.listen(PORT, () => {
     console.log(`${PORT} is on !!`);
-    
-})
+  });
+}
 
 export default app
