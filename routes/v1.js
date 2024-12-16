3b2e521886b5627d35e0e928edaa35e91dd6dbe6
h-e-l-e-n <leeleilei07@gmail.com>
2024-12-16 10:51:24 +0800

    fix

--- routes/v1.js
+++ routes/v1.js
@@ -2,9 +2,7 @@ import Express from 'express'
 import * as PaymentController from '../controllers/payment_controller.js'
 
 const v1Route = Express.Router()
-app.get('/', (req, res) => {
-    res.send('Welcome to the application')
-})
+
 v1Route.get('/payment', PaymentController.Payment)
 v1Route.post('/createOrder', PaymentController.CreateOrder)
 v1Route.get('/check/:id', PaymentController.CheckDetail)
